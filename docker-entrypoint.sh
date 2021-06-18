7c55dfd78678c3286366284973d993d582ffd782
Raymond Mak <raymond.mak@chargepoint.com>
2021-06-17 17:17:35 -0700

    Set root password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eo pipefail
 
+echo "root:$(head -c${1:-16} /dev/urandom | base64 | tr -dc _A-Z-a-z-0-9)"|chpasswd
+
 echo "$AUTHORIZED_KEYS" | base64 -d >/root/.ssh/authorized_keys
 
 chown root:root /root/.ssh/authorized_keys
